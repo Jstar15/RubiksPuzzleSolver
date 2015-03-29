59b08d26b7e35e069a8634ce99cc2d897e027386
Jstar15 <jordan_waddell@live.co.uk>
2015-03-29 00:07:30 +0000

    Added cube object which helps holds data on cube position

---
+++ RubiksPuzzleSolver/src/robotics/CubeObject.java
@@ -0,0 +1,85 @@
+package robotics;
+
+public class CubeObject {
+	private char front = 'F';
+	private char up = 'U';
+	private char right = 'R';
+	private char down = 'D'; 
+	private char left ='L';
+	private char back ='B';
+	
+	
+	public void FlipCube(){
+		char frontt = up;
+		char upt = back;
+		char downt = front; 
+		char backt = down;
+		
+		front = frontt;
+		up = upt;
+		down = downt;
+		back = backt;
+	}
+
+	public void MoveCube90AntiClockwise(){
+		char leftt = front;
+		char frontt = right;
+		char rightt = back; 
+		char backt = left;
+		
+		left = leftt;
+		front = frontt;
+		right = rightt;
+		back = backt;
+	}
+	
+	public void MoveCube90Clockwise(){
+		char leftt = back;
+		char frontt = left;
+		char rightt = front; 
+		char backt = right;
+		
+		left = leftt;
+		front = frontt;
+		right = rightt;
+		back = backt;
+	}
+
+	public void MoveCube180(){
+		char leftt = right;
+		char frontt = back;
+		char rightt = left; 
+		char backt = front;
+		
+		left = leftt;
+		front = frontt;
+		right = rightt;
+		back = backt;
+	}
+	
+	public char getFront() {
+		return front;
+	}
+
+	public char getUp() {
+		return up;
+	}
+	
+	public char getRight() {
+		return right;
+	}
+
+	public char getDown() {
+		return down;
+	}
+
+	public char getLeft() {
+		return left;
+	}
+
+	public char getBack() {
+		return back;
+	}
+	
+	
+}
